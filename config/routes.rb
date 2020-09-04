cd26028c919bc44572d20229f517198919cbefea
Niko Colom <shogun.endo@gmail.com>
2020-09-04 01:07:09 -0500

    Fix Games controller and view pluralization

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/', to: 'title#index', as: 'title'
 
-  resources :game, only: [:index, :show, :new, :create, :destroy]
+  resources :games, only: [:index, :show, :new, :create, :destroy]
   resources :player, only: [:show]
 end
